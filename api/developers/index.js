6137e4169d7f94c1f7d6a05d64532d9fe3e7b4e6
Sargis <shahinyan.sargis@gmail.com>
2018-07-08 21:32:11 +0400

    bug fixing

--- api/developers/index.js
+++ api/developers/index.js
@@ -5,7 +5,13 @@ const router = express.Router();
 const Users = require(`${APP_PATH}/models/users`);
 
 router.get('/', function(req, res) {
-	Users.getDevelopers().then(user => res.json(user), err => res.status(404).json(err));
+	if(res.locals.user.role === roles.manager) {
+		Users.getDevelopers().then(user => res.json(user), err => res.status(404).json(err));
+	} else {
+		return res.status(403).json({
+		    'message': 'Developer can not accept this resource'
+		});
+	}
 });
 
 module.exports = router;
